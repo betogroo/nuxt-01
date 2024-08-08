a71179849ec46bf90b66a1ec3c0b7962b4dd92f3
Luiz Humberto <luizhumberto@gmail.com>
2024-08-08 08:50:58 -0300

    feat: add blog page with parameters

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,7 @@
       <ul>
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
       </ul>
     </nav>
     <NuxtPage />
---
+++ app/pages/blog/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>All Blog posts here</div>
+</template>
