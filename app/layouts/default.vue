3995a8778fd54e3d7a64639364cf09b05a3e7232
Luiz Humberto <luizhumberto@gmail.com>
2024-08-09 07:35:00 -0300

    feat: stylize nav-bar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-        <ul>
-          <li v-for="id in 10">
-            <NuxtLink :to="`/blog/${id}`">Blog página {{ id }}</NuxtLink>
-          </li>
+  <div class="container mx-auto bg-gray-100  max-w-2xl">
+    <header class="flex justify-between items-center bg-blue-200 px-2">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold hover:bg-gray-200 p-2" >betogroo</NuxtLink>
+      </div>
+      <nav class="font-mono">
+        <ul class="flex space-x-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
         </ul>
-      </ul>
-    </nav>
-    <slot />
+      </nav>
+    </header>
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-const route = useRoute()
-definePageMeta({
-  layout: 'default',
-})
 
-const enableCustomLayout = () => {
-  setPageLayout(route.meta.layout === 'default' ? 'invert' : 'default')
-}
 </script>
 
 <template>
   <div>About</div>
-  <button @click="enableCustomLayout">Layout</button>
 </template>
